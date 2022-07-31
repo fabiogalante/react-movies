c6822599ff4df9168500049b92d9f7ac1d60f0a4
Fabio Galante Manse <fabiogalantemans@gmail.com>
2022-07-31 20:53:15 -0300

    canSee

--- src/DigitaCopia.tsx
+++ src/DigitaCopia.tsx
@@ -3,15 +3,28 @@ import {useState} from "react";
 export default function DigitaCopia() {
 
     const [text, setText] = useState('');
+    const [canSee, setCanSee] = useState(false);
 
     function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
         setText(e.currentTarget.value);
     }
 
+    function handleCheckboxChanged() {
+        setCanSee(!canSee)
+    }
+
     return (
-        <div>
+        <>
+            <h1>Events Example</h1>
             <input type="text" onKeyUp={(e) => handleKeyUp(e)} />
             <div>{text}</div>
-        </div>
+            <hr/>
+            <hr/>
+            <div>
+                <input type="checkbox" onChange={handleCheckboxChanged} />
+            </div>
+            {canSee ? <div>You are seeing the secret</div> :
+                <div>You are not allowed to see</div>}
+        </>
     )
 }
